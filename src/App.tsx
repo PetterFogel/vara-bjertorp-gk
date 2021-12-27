6e13739d7a01a023721ef8230283ae1fc5963ccf
Petter <petterfogel96@gmail.com>
2021-12-27 14:02:25 +0100

    Add the pages component + adjusted styling in index.css.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Pages from "./components/layout/pages/Pages";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Vara-Bjertorp GK</h1>
-    </div>
+    <>
+      <Pages />
+    </>
   );
 }
 
