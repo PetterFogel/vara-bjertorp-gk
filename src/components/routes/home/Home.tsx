936d1cd8cef8ef28b003bd7dd621cf9b034360b3
Petter <petterfogel96@gmail.com>
2021-12-30 13:08:35 +0100

    Add a content with a background and intro text + styling.

--- src/components/routes/home/Home.tsx
+++ src/components/routes/home/Home.tsx
@@ -1,7 +1,26 @@
-import React, { FC } from "react";
+import { FC } from "react";
+import { Link } from "react-router-dom";
+import Divider from "../../UI/divider/Divider";
+import classes from "./Home.module.css";
 
 const Home: FC = () => {
-  return <div>Home</div>;
+  return (
+    <section className={classes.section}>
+      <div className={classes.introContainer}>
+        <h1 className={classes.homeTitle}>
+          Varmt Välkommna till oss på <br /> Vara-Bjertorp GK
+        </h1>
+        <button className={classes.btn}>BOKA TID</button>
+        <Link to="/medlem" className={classes.memberTag}>
+          Inte medlem? Bli medlem här.
+        </Link>
+      </div>
+      <div className={classes.homeContainer}>
+        <Divider title="Om oss" />
+        <p>Test</p>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
