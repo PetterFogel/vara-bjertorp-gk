3b7a95f9fc497ccf1216f6ab3fe4ecf2758c44eb
Petter <petterfogel96@gmail.com>
2021-12-30 13:40:58 +0100

    Add some cards with link + styling.

--- src/components/routes/home/Home.tsx
+++ src/components/routes/home/Home.tsx
@@ -5,7 +5,7 @@ import classes from "./Home.module.css";
 
 const Home: FC = () => {
   return (
-    <section className={classes.section}>
+    <section>
       <div className={classes.introContainer}>
         <h1 className={classes.homeTitle}>
           Varmt Välkommna till oss på <br /> Vara-Bjertorp GK
@@ -17,7 +17,36 @@ const Home: FC = () => {
       </div>
       <div className={classes.homeContainer}>
         <Divider title="Om oss" />
-        <p>Test</p>
+        <p className={classes.paragraph}>
+          Vår bana invigdes 1990 och några år tidigare bildades klubben, närmare
+          bestämt 1987.
+          <br />
+          <br />
+          Banan ligger i en fantastisk slottsmiljö mitt på Västgötaslätten. Men
+          låt dig inte luras av det flacka landskapet. Banan ger alla en
+          utmaning, oavsett handicap.
+          <br />
+          <br />
+          Vi har av Golftidningen Golf Digest ranking av Sveriges bästa banor
+          röstats fram hela 9 gånger som Västergötlands bästa bana.
+        </p>
+        <div className={classes.boxContainer}>
+          <div className={classes.boxItem1}>
+            <Link to="/nyheter">
+              <button className={classes.boxBtn}>NYHETER</button>
+            </Link>
+          </div>
+          <div className={classes.boxItem2}>
+            <Link to="/boka-starttid">
+              <button className={classes.boxBtn}>BOKA TID</button>
+            </Link>
+          </div>
+          <div className={classes.boxItem3}>
+            <Link to="/medlem">
+              <button className={classes.boxBtn}>MEDLEM</button>
+            </Link>
+          </div>
+        </div>
       </div>
     </section>
   );
