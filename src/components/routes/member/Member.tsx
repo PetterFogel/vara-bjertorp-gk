db36b7737bb42afae5986596403b680a37454c6e
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-12-30 19:31:13 +0100

    Info and links  implemented

--- src/components/routes/member/Member.tsx
+++ src/components/routes/member/Member.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from "react";
-import { Link } from "react-router-dom";
 import Divider from "../../UI/divider/Divider";
 import classes from "./Member.module.css";
 
@@ -8,17 +7,104 @@ const Member: FC = () => {
     <section>
       <div className={classes.memberContainer}>
         <div className={classes.image}>
-          <div className={classes.title}>Bli medlem</div>
+          <h2 className={classes.title}>Bli medlem</h2>
         </div>
-        <p className={classes.paragraph}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sit
-          architecto, inventore qui corporis totam labore facere impedit
-          tempora, quis quidem cum, optio doloribus incidunt eum quam explicabo
-          rem veritatis.
+        <div className={classes.infoContainer}>
+          <p className={classes.paragraph}>
+            Vara-Bjertorp Golfklubb bildades 1987 och banan invigdes 1990 och är
+            en park och skogsbana i slottsmiljö med personlig service. Bli
+            medlem i Vara-Bjertorp GK – det finns olika alternativ och vi hoppas
+            något ska passa just dig. För mer info eller anmälan var god
+            kontakta kansliet på{" "}
+            <a
+              href="tel:+0512-20261"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={classes.link}
+            >
+              0512-20261
+            </a>{" "}
+            eller skriv e-post till info@vara-bjertorpgk.se{" "}
+            <a
+              href="mailto: info@vara-bjertorpgk.se"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={classes.link}
+            >
+              info@vara-bjertorpgk.se
+            </a>{" "}
+          </p>
           <br />
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sed
-          earum fugit tenetur, non magnam, placeat ducimus error u
-        </p>
+          <p className={classes.paragraph}>
+            Vi är den lilla klubben med en härlig stämning. Här är alla
+            välkomna! Med vårt fina klubbhus och vår fina och utmanande bana kan
+            alla känna sig som hemma här
+          </p>
+          <br />
+          <p className={classes.boldText}>
+            Som medlem i Vara-Bjertorp GK har du nytta av samarbeten som vi har
+            med andra klubbar. Dessa samarbeten gäller alla fullbetalande
+            medlemmar i klubben.
+          </p>
+          <br />
+          <ul className={classes.list}>
+            <li>
+              <a href="#" className={classes.link}>
+                Se våra årsavgifter
+              </a>
+            </li>
+            <li>
+              <a href="#" className={classes.link}>
+                Frågor? Kontakta vårt kansli
+              </a>
+            </li>
+          </ul>
+        </div>
+        <Divider title={"Medlemsavgifter 2022"}></Divider>
+        <div className={classes.optionContainer}>
+          <div className={classes.optionBox}>
+            <p>hello</p>
+            <p>test</p>
+            <p>hello</p>
+            <p>test</p>
+          </div>
+          <div className={classes.optionBox}>
+            <p>hello</p>
+            <p>test</p>
+            <p>hello</p>
+            <p>test</p>
+          </div>
+          <div className={classes.optionBox}>
+            <p>hello</p>
+            <p>test</p>
+            <p>hello</p>
+            <p>test</p>
+          </div>
+          <div className={classes.optionBox}>
+            <p>hello</p>
+            <p>test</p>
+            <p>hello</p>
+            <p>test</p>
+          </div>
+          <div className={classes.optionBox}>
+            <p>hello</p>
+            <p>test</p>
+            <p>hello</p>
+            <p>test</p>
+          </div>
+          <div className={classes.optionBox}>
+            <p>hello</p>
+            <p>test</p>
+            <p>hello</p>
+            <p>test</p>
+          </div>
+          <div className={classes.optionBox}>
+            <p>hello</p>
+            <p>test</p>
+            <p>hello</p>
+            <p>test</p>
+          </div>
+        </div>
       </div>
     </section>
   );
