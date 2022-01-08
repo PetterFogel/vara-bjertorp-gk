5754087aa41e4cd68c3b0cc4ac6251975c9b38c6
Oliver Johansson <oliverjohansson3@gmail.com>
2022-01-08 14:53:41 +0100

    Added content for news and fixed img bug

--- src/components/routes/news/News.tsx
+++ src/components/routes/news/News.tsx
@@ -3,27 +3,40 @@ import classes from "./News.module.css";
 import posts from './assets/news.json'
 import { SingleNews } from "./assets/types";
 import { NewsContainer } from "./components/NewsContainer";
+import Divider from "../../UI/divider/Divider";
 
 const News: FC = () => {
-  const lastPost = posts.news.slice(-1)[0] 
-  console.log(lastPost)
-  const allButLast = posts.news.slice(0, -1);
-  console.log(allButLast)
+  const postsArray = posts.news
+  const lastPost = postsArray.slice(-1)[0] 
+  const oldPosts = postsArray.slice(0, -5) 
+  const latestOldPosts = oldPosts.slice(Math.max(oldPosts.length - 3, 0))
+  const latestPosts = postsArray.slice(Math.max(postsArray.length - 5, 0))
+  const allButLast = latestPosts.slice(0, -1);
   return(
     <section>
       <div className={classes.newsContainer}>
-        <div className={classes.image}>
-          <h2 className={classes.title}>Nyheter</h2>
+        <div className={classes.recentNewsContainer}>
+          <div className={classes.image}>
+            <h2 className={classes.title}>Nyheter</h2>
+          </div>
+          <div className={classes.mappedValuesContainer}>
+            <div className={classes.lastPostContainer}>
+              <NewsContainer withContent isBig isImage news={lastPost}/>
+            </div>
+            <div className={classes.oldPostContainer}>
+              {allButLast.map((post: SingleNews) => (
+                <NewsContainer withContent news={post} />
+                ))}
+            </div>
+          </div>
         </div>
-        <div className={classes.mappedValuesContainer}>
-          <div className={classes.lastPostContainer}>
-            <NewsContainer news={lastPost}/>
-          </div>
-          <div className={classes.oldPostContainer}>
-            {allButLast.map((post: SingleNews) => (
-              <NewsContainer news={post} />
-            ))}
-          </div>
+        <section style={{marginBottom: '5rem'}}>
+          <Divider title={"Äldre nyheter"}></Divider>
+        </section>
+        <div className={classes.oldNewsContainer}>
+          {latestOldPosts.map((post: SingleNews) => (
+            <NewsContainer isMargin isImage news={post} />
+          ))}
         </div>
       </div>
     </section>
