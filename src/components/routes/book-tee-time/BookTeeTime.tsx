cffc9605a5f4c27c37feec68bab7f7d73238c958
Oliver Johansson <oliverjohansson3@gmail.com>
2022-01-08 12:29:15 +0100

    Added basic content for using GIT

--- src/components/routes/book-tee-time/BookTeeTime.tsx
+++ src/components/routes/book-tee-time/BookTeeTime.tsx
@@ -1,7 +1,15 @@
 import React, { FC } from "react";
+import { Helmet } from 'react-helmet'
 
 const BookTeeTime: FC = () => {
-  return <div>BookTeeTime</div>;
+  return <div>
+    <Helmet>
+      {/* <script type="text/javascript" src="https://gitwidgets.golf.se/Widget/script.js"></script>
+      <script type="text/javascript">
+        {GITWidget.addGolfLogin({ width: 250, guid: 'ceda30cf-309d-418e-b329-82a167a661ab' })};
+      </script> */}
+    </Helmet>
+  </div>;
 };
 
 export default BookTeeTime;
