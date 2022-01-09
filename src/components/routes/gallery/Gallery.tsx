1f67635641d23a03d0b5a5603b057f4cb47d13d8
Hampus Olausson <hampus.john.olausson@gmail.com>
2022-01-09 19:39:29 +0100

    Completed the gallery page

--- src/components/routes/gallery/Gallery.tsx
+++ src/components/routes/gallery/Gallery.tsx
@@ -8,60 +8,47 @@ const Gallery: FC = () => {
       <div className={classes.galleryContainer}>
         <h2 className={classes.title}>Galleri</h2>
         <div className={classes.container}>
-          <div className={classes.imageContainer} id={classes.imageOne}>
-          </div>
-          <div className={classes.imageContainer} id={classes.imageTwo}>
-          </div>
+          <div className={classes.imageContainer} id={classes.imageOne}></div>
+          <div className={classes.imageContainer} id={classes.imageTwo}></div>
         </div>
         <div className={classes.infoContainer}>
-            <p className={classes.paragraph}>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque
-              similique vitae sint aut! Aspernatur vitae possimus, quo iste
-              distinctio temporibus magni sapiente vero? Ea officiis nihil
-              voluptatem neque veniam similique?
-            </p>  
+          <Divider title={"Golfbanan"}></Divider>
+          <p className={classes.paragraph}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque
+            similique vitae sint aut! Aspernatur vitae possimus, quo iste
+            distinctio temporibus magni sapiente vero? Ea officiis nihil
+            voluptatem neque veniam similique?
+          </p>
         </div>
         <div className={classes.container}>
-          <div className={classes.imageContainer} id={classes.imageThree}>
-          </div>
-          <div className={classes.imageContainer} id={classes.imageFour}>
-          </div>
+          <div className={classes.imageContainer} id={classes.imageThree}></div>
+          <div className={classes.imageContainer} id={classes.imageFour}></div>
         </div>
         <div className={classes.infoContainer}>
-            <p className={classes.paragraph}>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque
-              similique vitae sint aut! Aspernatur vitae possimus, quo iste
-              distinctio temporibus magni sapiente vero? Ea officiis nihil
-              voluptatem neque veniam similique?
-            </p>  
+          <Divider title={"Restaurangen"}></Divider>
+          <p className={classes.paragraph}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque
+            similique vitae sint aut! Aspernatur vitae possimus, quo iste
+            distinctio temporibus magni sapiente vero? Ea officiis nihil
+            voluptatem neque veniam similique?
+          </p>
         </div>
         <div className={classes.container}>
-          <div className={classes.imageContainer} id={classes.imageFive}>
-          </div>
-          <div className={classes.imageContainer} id={classes.imageSix}>
-          </div>
+          <div className={classes.imageContainer} id={classes.imageFive}></div>
+          <div className={classes.imageContainer} id={classes.imageSix}></div>
         </div>
         <div className={classes.infoContainer}>
-            <p className={classes.paragraph}>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque
-              similique vitae sint aut! Aspernatur vitae possimus, quo iste
-              distinctio temporibus magni sapiente vero? Ea officiis nihil
-              voluptatem neque veniam similique?
-            </p>  
+          <Divider title={"Slottet"}></Divider>
+          <p className={classes.paragraph}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque
+            similique vitae sint aut! Aspernatur vitae possimus, quo iste
+            distinctio temporibus magni sapiente vero? Ea officiis nihil
+            voluptatem neque veniam similique?
+          </p>
         </div>
         <div className={classes.container}>
-          <div className={classes.imageContainer} id={classes.imageSeven}>
-          </div>
-          <div className={classes.imageContainer} id={classes.imageEight}>
-          </div>
-        </div>
-        <div className={classes.infoContainer}>
-            <p className={classes.paragraph}>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque
-              similique vitae sint aut! Aspernatur vitae possimus, quo iste
-              distinctio temporibus magni sapiente vero? Ea officiis nihil
-              voluptatem neque veniam similique?
-            </p>  
+          <div className={classes.imageContainer} id={classes.imageSeven}></div>
+          <div className={classes.imageContainer} id={classes.imageEight}></div>
         </div>
       </div>
     </section>
