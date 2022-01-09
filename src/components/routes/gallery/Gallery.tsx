d4fe0055153e685f806a9ac098654565e7ffc282
Hampus Olausson <hampus.john.olausson@gmail.com>
2022-01-09 18:54:31 +0100

    Basic layout added

--- src/components/routes/gallery/Gallery.tsx
+++ src/components/routes/gallery/Gallery.tsx
@@ -1,7 +1,29 @@
 import React, { FC } from "react";
+import classes from "./Gallery.module.css";
+import Divider from "../../UI/divider/Divider";
 
 const Gallery: FC = () => {
-  return <div>Gallery</div>;
+  return (
+    <section>
+      <div className={classes.galleryContainer}>
+        <h2 className={classes.title}>Galleri</h2>
+        <div className={classes.container}>
+          <div className={classes.imageContainerOne} id={classes.imageOne}>
+          </div>
+          <div className={classes.imageContainerOne}>
+          </div>
+        </div>
+        <div className={classes.container}>
+            <p className={classes.paragraph}>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque
+              similique vitae sint aut! Aspernatur vitae possimus, quo iste
+              distinctio temporibus magni sapiente vero? Ea officiis nihil
+              voluptatem neque veniam similique?
+            </p>  
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Gallery;
