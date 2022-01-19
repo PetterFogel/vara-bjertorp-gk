626e9e5f1af16f9ba7229eda9c7070efdd58e4d6
Hampus Olausson <hampus.john.olausson@gmail.com>
2022-01-19 20:27:21 +0100

    Hadnling errors in login

--- src/components/routes/login/Login.tsx
+++ src/components/routes/login/Login.tsx
@@ -1,45 +1,37 @@
 import axios from "axios";
 import React, { FC, SyntheticEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
 // import { Redirect } from "react-router";
 import classes from "./Login.module.css";
 
 const Login: FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  // const [redirect, setRedirect] = useState(false);
-
-  // const submit = async (e: SyntheticEvent) => {
-  //   const response = await axios.post(
-  //     `${process.env.REACT_APP_API_URL}api/login`,
-  //     {
-  //       email: email,
-  //       password: password,
-  //     }
-  //   );
-  //   console.log(response);
-
-  //   // setRedirect(true);
-  // };
+  const history = useNavigate();
 
   const submit = async () => {
+    try {
     const response = await axios.post(
-      `${process.env.REACT_APP_API_URL}api/login`,
+      `${process.env.REACT_APP_API_URL}api/Login`,
       {
-        headers: {
-          "content-type": "application/json",
-        },
         data: {
           email: email,
           password: password,
         },
+      },
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        },
       }
     );
-    console.log(response)
+    console.log(response.data);
+    history('/');
+    }
+    catch(error){
+      console.log(error)
+    }
   };
-
-  // if (redirect) {
-  //   // return <Redirect to="/"/>
-  // }
 
   return (
     <section>
@@ -47,6 +39,7 @@ const Login: FC = () => {
         <form onSubmit={submit} className={classes.inputContainer}>
           <h2 className={classes.loginTitle}>Logga in</h2>
           <input
+            required
             type="email"
             placeholder="E-mail..."
             className={classes.input}
@@ -57,6 +50,7 @@ const Login: FC = () => {
           />
 
           <input
+            required
             type="password"
             placeholder="Lösenonord..."
             className={classes.input}
@@ -65,7 +59,9 @@ const Login: FC = () => {
               setPassword(e.target.value)
             }
           />
-          <button className={classes.button}>Logga in</button>
+          <button className={classes.button}>
+            Logga in
+          </button>
         </form>
       </div>
     </section>
