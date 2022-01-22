edfabec5c48232beb31b6742977239b8486ef7f7
Petter <petterfogel96@gmail.com>
2022-01-22 13:03:35 +0100

    Add boolean state to check if admin is logged in.

--- src/components/Contexts/adminContext.tsx
+++ src/components/Contexts/adminContext.tsx
@@ -1,59 +1,69 @@
 import axios from "axios";
-// import React, { createContext, FC, useState } from "react";
-// import { useNavigate } from "react-router-dom";
+import React, { createContext, FC, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-// export interface Admin {
-//   id: string;
-//   email: string;
-//   password: string;
-// }
+export interface Admin {
+  id: string;
+  email: string;
+  password: string;
+}
 
-// export interface Options {
-//   login: () => void;
-//   logout: () => void;
-// }
+export interface Options {
+  login: () => void;
+  logout: () => void;
+}
 
-// export const AdminContext = createContext<Options>({
-//   login: () => {},
-//   logout: () => {},
-// });
+export const AdminContext = createContext<Options>({
+  login: () => {},
+  logout: () => {},
+});
 
-// const AdminProvider: FC<{}> = ({ children }) => {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const history = useNavigate();
+const AdminProvider: FC<{}> = ({ children }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>();
+  const history = useNavigate();
 
-//   const login = async () => {
-//     try {
-//       const response = await axios.post(
-//         `${process.env.REACT_APP_API_URL}api/Login`,
-//         {
-//           data: {
-//             email: email,
-//             password: password,
-//           },
-//         },
-//         {
-//           headers: {
-//             "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-//           },
-//         }
-//       );
-//       console.log(response.data);
-//       history("/");
-//     } catch (error) {
-//       console.log(error);
-//       <div>
-//         <p>Den angav fel E-postadress eller lösenord</p>
-//       </div>;
-//     }
-//   };
+  const login = async () => {
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_URL}api/Login`,
+        {
+          data: {
+            email: email,
+            password: password,
+          },
+        },
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+          },
+        }
+      );
+      console.log(response.data);
+      history("/");
+    } catch (error) {
+      console.log(error);
+      <div>
+        <p>Den angav fel E-postadress eller lösenord</p>
+      </div>;
+    }
+  };
 
-//   return (
-//     <AdminContext.Provider value={{ login, logout }}>
-//       {children}
-//     </AdminContext.Provider>
-//   );
-// };
+  const logout = async () => {
+    console.log("Logout!");
+  };
 
-// export default AdminProvider;
+  const ContextValue: Options = {
+    login,
+    logout,
+  };
+
+  return (
+    <AdminContext.Provider value={ContextValue}>
+      {children}
+    </AdminContext.Provider>
+  );
+};
+
+export default AdminProvider;
