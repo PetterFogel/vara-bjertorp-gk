8f84f4934517895a17321c75dba981ee61d5e96a
Petter <petterfogel96@gmail.com>
2022-01-22 12:57:49 +0100

    Add icon button for admin to be navigated to create posts

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,7 +1,8 @@
 import { FC, useEffect, useState } from "react";
 import classes from "./Header.module.css";
 import logoImg from "../../../assets/small-logo.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { MdCreate } from "react-icons/md";
 
 interface HeaderProps {
   onToggleMenuChange: () => void;
@@ -9,6 +10,7 @@ interface HeaderProps {
 
 const Header: FC<HeaderProps> = ({ onToggleMenuChange }: HeaderProps) => {
   const [offset, setOffset] = useState(0);
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.onscroll = () => {
@@ -33,11 +35,18 @@ const Header: FC<HeaderProps> = ({ onToggleMenuChange }: HeaderProps) => {
           <h3 className={classes.logo}>Vara-Bjertop GK</h3>
         </div>
       </Link>
-      <div className={classes.burgerMenu} onClick={onToggleMenuChange}>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+      <nav className={classes.nav}>
+        <MdCreate
+          className={classes.postIcon}
+          size={30}
+          onClick={() => navigate("/create-posts")}
+        />
+        <div className={classes.burgerMenu} onClick={onToggleMenuChange}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </nav>
     </header>
   );
 };
