4b40d235a060b38e42236e622327348529bdfc69
Petter <petterfogel96@gmail.com>
2022-01-25 20:04:53 +0100

    Implemnted image

--- src/components/UI/intro-container/IntroContainer.tsx
+++ src/components/UI/intro-container/IntroContainer.tsx
@@ -1,17 +1,20 @@
 import { FC } from "react";
 import classes from "./IntroContainer.module.css";
-import image from "../../../assets/DJI_0169_sign.jpg";
 
 type IntroContainerProps = {
   title: string;
   imageUrl: string;
 };
 
-const IntroContainer: FC<IntroContainerProps> = ({ title }) => {
+const IntroContainer: FC<IntroContainerProps> = ({ title, imageUrl }) => {
   return (
     <div className={classes.container}>
       <h1 className={classes.title}>{title}</h1>
-      <img className={classes.imageStyle} src={image} alt="" />
+      <img
+        className={classes.imageStyle}
+        src={imageUrl}
+        alt="backgroundimage"
+      />
     </div>
   );
 };
